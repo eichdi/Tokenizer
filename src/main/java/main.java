2fedd57832fe0075927fce0855b3bde74f839b69
eichdi <eichdi96@gmail.com>
2016-05-10 16:12:27 +0300

    Refactor main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,7 @@
 import tokenizer.Tokenizer;
 import tokenizer.TokenizerImpl;
 
-public class main {
+public class Main {
 
     public static void main(String[] args) {
         String text = "sluchaini tekst dlya Marselya";
