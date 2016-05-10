853ed813a79188a1d534765b5ddef4677252548a
eichdi <eichdi96@gmail.com>
2016-05-10 17:03:53 +0300

    Refactor test

--- src/test/java/tokenizer/TokenizerImplTest.java
+++ src/test/java/tokenizer/TokenizerImplTest.java
@@ -1,20 +1,41 @@
 package tokenizer;
 
+import org.junit.Before;
+
 import java.util.stream.Stream;
 
 import static org.junit.Assert.*;
 
 
-public class TokenizerImplTest extends TokenizerImpl {
+public class TokenizerImplTest  {
 
-    public void TestDivided(String[] expected, String[] actual, String by){
+    TokenizerImpl test_class;
+    String[] test_word;
+    String test_text;
+    String by;
+    char by_char;
+
+
+    @Before
+    public void setAp(){
+        this.test_class = new TokenizerImpl();
+        this.test_word = new String[]{"Тестовое", "слово"};
+        this.by_char = ';';
+        this.by = ""+by_char;
+        this.test_text = getText(by);
+    }
+
+    public  String getText(String by){
+        String test_text = test_word[0];
+        for(int i=1;i<test_word.length;i++){
+            test_text+=by+test_word[i];
+        }
+        return test_text;
+    }
+
+    public void testDivided(String[] expected, String[] actual, String by){
         if(expected.length==actual.length){
-
-            for(int i=0;i<expected.length;i++){
-                if(!expected[i].equals(actual[i])){
-                    fail();
-                }
-            }
+            assertArrayEquals("Word is wrong divided",expected,actual);
         }
         else{
             fail();
@@ -23,26 +44,18 @@ public class TokenizerImplTest extends TokenizerImpl {
 
     @org.junit.Test
     public void testDividedBy() throws Exception {
-        String test_text = "Тестовое,слово";
-        TokenizerImpl test_class = new TokenizerImpl();
-        String[] test_word = {"Тестовое","слово"};
-        TestDivided(test_class.dividedBy(",",test_text),test_word,",");
+        testDivided(test_class.dividedBy(by,test_text),test_word,by);
     }
 
     @org.junit.Test
     public void testDividedBy1() throws Exception {
-        String test_text = "Тестовое;слово";
-        TokenizerImpl test_class = new TokenizerImpl();
-        String[] test_word = {"Тестовое","слово"};
-        TestDivided(test_class.dividedBy(';',test_text),test_word,",");
-
+        testDivided(test_class.dividedBy(by_char,test_text),test_word,by);
     }
 
     @org.junit.Test
     public void testSplit() throws Exception {
         String test_text = "Тестовое слово";
-        TokenizerImpl test_class = new TokenizerImpl();
-        String[] test_word = {"Тестовое","слово"};
-        TestDivided(test_class.split(test_text),test_word,",");
+        String[] test_word = new String[]{"Тестовое", "слово"};
+        testDivided(test_class.split(test_text),test_word," ");
     }
 }
